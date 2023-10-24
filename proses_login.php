136997593e6c0d31f26945daa13b2ce0a46e9220
MOH. DANI KURNIAWAN SUGIARTO <125776654+TamarillowBonaparte@users.noreply.github.com>
2023-10-24 09:06:06 +0700

    proseslogin
    bug fix

---
+++ proses_login.php
@@ -0,0 +1,155 @@
+<?php
+session_start();
+require('koneksi.php');
+
+if(isset($_POST['submit'])){
+    $email = $_POST['txt_email'];
+    $pass = $_POST['txt_pass'];
+
+    if(!empty(trim($email)) && !empty(trim($pass))){
+
+        //select data berdasarkan username dari database
+        $query = "SELECT * FROM user_detail WHERE user_email = '$email'";
+        $result = mysqli_query($koneksi, $query);
+        $num = mysqli_num_rows($result);
+
+        if ($num != 0) {
+            $row = mysqli_fetch_array($result);
+            $userVal = $row['user_email'];
+            $passVal = $row['user_password'];
+            $userName = $row['user_fullname'];
+            $level = $row['level'];
+
+            if($userVal == $email && $passVal == $pass){
+                // Set session untuk pengguna yang berhasil login
+                $_SESSION['user_email'] = $email;
+                $_SESSION['user_fullname'] = $userName;
+                $_SESSION['level'] = $level;
+
+                header('Location: index.php'); // Ganti dengan halaman yang sesuai
+            } else {
+                $error = 'User atau password salah!!';
+                header('Location: proses_login.php?error=' . urlencode($error));
+            }
+        } else {
+            $error = 'User tidak ditemukan';
+            header('Location: proses_login.php?error=' . urlencode($error));
+        }
+    } else {
+        $error = 'Data tidak boleh kosong';
+        header('Location: proses_login.php?error=' . urlencode($error));
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <?php
+                                    // Menampilkan pesan kesalahan jika ada
+                                    if (isset($_GET['error'])) {
+                                        $error = urldecode($_GET['error']);
+                                        echo '<div class="alert alert-danger">'.$error.'</div>';
+                                    }
+                                    ?>
+                                    <form class="user" method="POST" action="proses_login.php">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address..." name="txt_email">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password" name="txt_pass">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <button type="submit" name="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                        <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.php">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,160 @@
+<?php
+require('koneksi.php'); // Include your database connection script
+
+if (isset($_POST['register'])) {
+    // Retrieve user input from the form
+    $firstName = $_POST['txt_nama'];
+    $lastName = $_POST['txt_nama'];
+    $userEmail = $_POST['txt_email'];
+    $userPassword = $_POST['txt_pass'];
+
+    // You should perform input validation and sanitation here
+
+    // Hash the user's password for security (you may want to use a more secure method)
+    $hashedPassword = password_hash($userPassword, PASSWORD_DEFAULT);
+
+    // SQL query to insert user data into the database
+    $query = "INSERT INTO user_detail VALUES ('', '$userEmail', '$userPassword', '$userName', 2)";
+
+    // Execute the query
+    $result = mysqli_query($koneksi, $query);
+
+    if ($result) {
+        // Registration successful, redirect to login page
+        header('Location: proses_login.php');
+        exit;
+    } else {
+        // Registration failed, handle the error (e.g., display an error message)
+        echo "Registration failed. Please try again later.";
+    }
+}
+?>
+
+<!-- Your HTML registration form -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Your head content here -->
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <!-- ... Rest of your HTML form ... -->
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Your JavaScript imports and scripts here -->
+</body>
+
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Register</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <form class="user" method="POST" action="register.php">
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" class="form-control form-control-user" id="exampleFirstName"
+                                            placeholder="First Name" name="txt_nama">
+                                    </div>
+                                    
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                                        placeholder="Email Address" name="txt_email">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="exampleInputPassword" placeholder="Password" name="txt_pass">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="exampleRepeatPassword" placeholder="Repeat Password" name="txt_repeat_pass">
+                                    </div>
+                                </div>
+                                <button type="submit" name="register" class="btn btn-primary btn-user btn-block">
+                                    Register Account
+                                </button>
+                                <hr>
+                                <a href="index.html" class="btn btn-google btn-user btn-block">
+                                    <i class="fab fa-google fa-fw"></i> Register with Google
+                                </a>
+                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                    <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                                </a>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="proses_login.php">Already have an account? Login!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
