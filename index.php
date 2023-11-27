7fe8ec8997cd2521e029c4872f63b3a37aa34e97
Zul2306 <148838510+Zul2306@users.noreply.github.com>
2023-11-28 00:39:27 +0700

    push hari ini

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
     $pass = $_POST['txt_pass'];
     $emailCheck = mysqli_real_escape_string($koneksi, $email);
     $passCheck = mysqli_real_escape_string($koneksi, $pass);
-    
+
     if (!empty(trim($email)) && !empty(trim($pass))) {
 
         //select data berdasarkan username dari database
@@ -39,102 +39,94 @@ if (isset($_POST['submit'])) {
         }
     } else {
         $error = 'Data tidak boleh kosong';
-       // echo $error;
+        // echo $error;
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
-    <title>DIFANA POS</title>
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
 
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <!-- <link href="css/login.css" rel="stylesheet"> -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
-    
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Login #2</title>
+
+
+
 
 </head>
 
-<body class="bg-gradient-primary">
 
-    <div class="container">
+<body>
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
+    <div class="d-lg-flex half">
+        <div class="bg order-1 order-md-2" style="background-image: url(img/jajan.jpg);"></div>
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="contents order-2 order-md-1">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form action="index.php" method="post">
-                                            <div class="form-floating mb-3">
-                                            <label for="inputEmail">Username</label>
-                                                <input class="form-control" type="text" placeholder="username" name="txt_username"/>
-                                                <!-- <label for="inputEmail">Username</label> -->
-                                                <div  class=""><?php global $error; echo $error ?></div>
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                            <label for="inputPassword">Password</label>
-                                                <input class="form-control" type="password" placeholder="password" name="txt_pass"/>
-                                                <!-- <label for="inputPassword">Password</label> -->
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <a class="small" href="password.html">Lupa Password?</a>
-                                                <button class="btn btn-success fs-5" type="submit" name="submit">Login</button>
-                                            </div>
-                                        </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
-                                    </div>
+            <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    <!-- Nested Row within Card Body -->
+                    <div class="col-md-7">
+                        <h3>Login to <strong>SIDINAF</strong></h3>
+                        <p class="mb-4">Welcome back! Please enter your login information.</p>
+                        <form action="index.php" method="post">
+                            <div class="form-group first">
+                                <label for="inputEmail">Username</label>
+                                <input class="form-control" type="text" placeholder="username" name="txt_username" />
+                                <!-- <label for="inputEmail">Username</label> -->
+                                <div class="">
+                                    <?php global $error;
+                                    echo $error ?>
                                 </div>
                             </div>
+                            <div class="form-floating mb-3">
+                                <label for="inputPassword">Password</label>
+                                <input class="form-control" type="password" placeholder="password" name="txt_pass" />
+                                <!-- <label for="inputPassword">Password</label> -->
+                            </div>
+                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                <a class="small" href="password.html">Lupa Password?</a>
+                                <button class="btn btn-block btn-primary" type="submit" name="submit">Login</button>
+                            </div>
+                        </form>
+                        <hr>
+                        <div class="text-center">
+                            <a class="small" href="forgot-password.html">Forgot Password?</a>
+                        </div>
+                        <div class="text-center">
+                            <a class="small" href="register.html">Create an Account!</a>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
+    </div>
 
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </div>
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    </div>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
